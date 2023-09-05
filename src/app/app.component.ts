aa0bc64d49f4fce8c780dfcd05e1a4105939d530
salaahl <sokhona.salaha@gmail.com>
2023-09-06 00:03:46 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,37 @@
 import { Component } from '@angular/core';
 import * as questionBank from './questions/questionBank/questions';
-import * as animation from './animations/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-  
 export class AppComponent {
-  title = 'Quizz';
+  title: string = 'Quizz';
 
   // Le traitement de cette variable se trouve dans le questionsComponent
   questions = questionBank;
   show: boolean = true;
 
-  questionsCatalog: any[] = [
+  // Le clic sur un bouton injectera un catalogue dans cette variable
+  questionsCatalog: any = null;
+
+  catalogs: any[] = [
+    {
+      name: 'Lorem',
+      questions: this.questions.questionsLorem,
+      show: this.show,
+    },
     {
       name: 'Securité routière',
-      show: false,
+      questions: this.questions.securiteRoutiere,
+      show: this.show,
     },
   ];
 
   showCatalog(catalogSwitch: boolean) {
     this.show = catalogSwitch;
-    this.questionsCatalog.forEach((catalog) => {
+    this.catalogs.forEach((catalog) => {
       catalog.show = false;
     });
   }
--- src/app/questions/questionBank/questions.ts
+++ src/app/questions/questionBank/questions.ts
@@ -3,28 +3,25 @@
 export const securiteRoutiere = [
   {
     question_index: 0,
-    title: 'Après avoir consommé de l’alcool, boire un café serré fera-t-il baisser l’alcoolémie ?',
-    answers: [
-      'Oui',
-      'Non'
-    ],
+    title:
+      'Après avoir consommé de l’alcool, boire un café serré fera-t-il baisser l’alcoolémie ?',
+    answers: ['Oui', 'Non'],
     right_answer: {
       index: 1,
-      explanation: 'Rien ne permet d’accélérer l’élimination de l’alcool dans le sang, il faut juste attendre que le taux baisse et ça baisse beaucoup plus lentement qu\'on l\'imagine.'
+      explanation:
+        "Rien ne permet d’accélérer l’élimination de l’alcool dans le sang, il faut juste attendre que le taux baisse et ça baisse beaucoup plus lentement qu'on l'imagine.",
     },
     done: false,
   },
   {
     question_index: 1,
-    title: 'Entre 0,5 et 0,8 g d’alcool par litre de sang, soit à partir de 2 verres, le risque d’être responsable d’un accident mortel est multiplié en moyenne par :',
-    answers: [
-      '2,5',
-      '4,8',
-      '6,4',
-    ],
+    title:
+      'Entre 0,5 et 0,8 g d’alcool par litre de sang, soit à partir de 2 verres, le risque d’être responsable d’un accident mortel est multiplié en moyenne par :',
+    answers: ['2,5', '4,8', '6,4'],
     right_answer: {
       index: 2,
-      explanation: 'Champ visuel rétréci, perception des distances modifiée… L’alcool a des effets immédiats sur le cerveau. Il n’y a pas de petite dose : dès le seuil de 0,5 g par litre de sang, les risques sont réels.'
+      explanation:
+        'Champ visuel rétréci, perception des distances modifiée… L’alcool a des effets immédiats sur le cerveau. Il n’y a pas de petite dose : dès le seuil de 0,5 g par litre de sang, les risques sont réels.',
     },
     done: false,
   },
@@ -38,7 +35,8 @@ export const securiteRoutiere = [
     ],
     right_answer: {
       index: 2,
-      explanation: 'La nuit, l’alcool est présent dans la moitié des accidents mortels et atteint 66 % le week-end !'
+      explanation:
+        'La nuit, l’alcool est présent dans la moitié des accidents mortels et atteint 66 % le week-end !',
     },
     done: false,
   },
@@ -53,134 +51,122 @@ export const securiteRoutiere = [
     ],
     right_answer: {
       index: 3,
-      explanation: 'Peu importe la boisson alcoolisée, un verre (dose de bar) contient la même quantité d’alcool pur.'
+      explanation:
+        'Peu importe la boisson alcoolisée, un verre (dose de bar) contient la même quantité d’alcool pur.',
     },
     done: false,
   },
   {
     question_index: 4,
-    title: 'Dans certains cas, l’installation d’un éthylotest antidémarrage peut être proposée en alternative à une suspension de permis :',
-    answers: [
-      'Vrai',
-      'Faux',
-    ],
+    title:
+      'Dans certains cas, l’installation d’un éthylotest antidémarrage peut être proposée en alternative à une suspension de permis :',
+    answers: ['Vrai', 'Faux'],
     right_answer: {
       index: 0,
-      explanation: 'Cette alternative, prise par décision préfectorale, concerne les automobilistes contrôlés avec une alcoolémie délictuelle (supérieure à 0,8 g par litre de sang), pour une durée d’un an maximum (prolongeable jusqu’à 5 ans par l’autorité judiciaire). Ce dispositif préserve le droit de conduire tout en luttant contre l’alcool au volant.'
+      explanation:
+        'Cette alternative, prise par décision préfectorale, concerne les automobilistes contrôlés avec une alcoolémie délictuelle (supérieure à 0,8 g par litre de sang), pour une durée d’un an maximum (prolongeable jusqu’à 5 ans par l’autorité judiciaire). Ce dispositif préserve le droit de conduire tout en luttant contre l’alcool au volant.',
     },
     done: false,
   },
   {
     question_index: 5,
-    title: 'Les conducteurs impliqués dans un accident mortel avec un taux d’alcool positif sont en très grande majorité :',
-    answers: [
-      'Des hommes',
-      'Des femmes',
-    ],
+    title:
+      'Les conducteurs impliqués dans un accident mortel avec un taux d’alcool positif sont en très grande majorité :',
+    answers: ['Des hommes', 'Des femmes'],
     right_answer: {
       index: 0,
-      explanation: '92 % des conducteurs impliqués dans un accident mortel avec l’alcool sont des hommes. Une alcoolémie excessive est présente dans 30 % des accidents mortels.'
+      explanation:
+        '92 % des conducteurs impliqués dans un accident mortel avec l’alcool sont des hommes. Une alcoolémie excessive est présente dans 30 % des accidents mortels.',
     },
     done: false,
   },
   {
     question_index: 6,
-    title: 'Un conducteur titulaire du permis probatoire peut consommer avant de conduire :',
-    answers: [
-      '0 verre d’alcool',
-      '1 verre d’alcool',
-      '2 verres d’alcool',
-    ],
+    title:
+      'Un conducteur titulaire du permis probatoire peut consommer avant de conduire :',
+    answers: ['0 verre d’alcool', '1 verre d’alcool', '2 verres d’alcool'],
     right_answer: {
       index: 0,
-      explanation: 'Les accidents de la route sont la première cause de mortalité et de handicap des 18-25 ans et dans un quart de ces accidents, une alcoolémie excessive en est la cause. Pour les conducteurs titulaires d’un permis probatoire, la limite est de 0,2 g par litre de sang (0,1 mg par litre d’air expiré). 0,2 g par litre, c’est zéro verre d’alcool car cette limite peut être dépassée dès le premier verre d’alcool.'
+      explanation:
+        'Les accidents de la route sont la première cause de mortalité et de handicap des 18-25 ans et dans un quart de ces accidents, une alcoolémie excessive en est la cause. Pour les conducteurs titulaires d’un permis probatoire, la limite est de 0,2 g par litre de sang (0,1 mg par litre d’air expiré). 0,2 g par litre, c’est zéro verre d’alcool car cette limite peut être dépassée dès le premier verre d’alcool.',
     },
     done: false,
   },
   {
     question_index: 7,
-    title: 'Dans 1 accident mortel sur 5, un conducteur est positif aux stupéfiants.',
-    answers: [
-      'Vrai',
-      'Faux',
-    ],
+    title:
+      'Dans 1 accident mortel sur 5, un conducteur est positif aux stupéfiants.',
+    answers: ['Vrai', 'Faux'],
     right_answer: {
       index: 0,
-      explanation: 'Vrai. En 2021, près de 20 % des personnes décédées sur les routes ont été tuées dans un accident impliquant un conducteur sous l’emprise de stupéfiants. Cette part atteint 1 accident mortel sur 3 la nuit au cours des week-ends.'
+      explanation:
+        'Vrai. En 2021, près de 20 % des personnes décédées sur les routes ont été tuées dans un accident impliquant un conducteur sous l’emprise de stupéfiants. Cette part atteint 1 accident mortel sur 3 la nuit au cours des week-ends.',
     },
     done: false,
   },
   {
     question_index: 8,
-    title: 'Par combien le mélange cannabis/alcool multiplie le risque de causer un accident mortel ?',
-    answers: [
-      '7',
-      '18',
-      '29',
-    ],
+    title:
+      'Par combien le mélange cannabis/alcool multiplie le risque de causer un accident mortel ?',
+    answers: ['7', '18', '29'],
     right_answer: {
       index: 2,
-      explanation: 'Le mélange drogue/cannabis est délétère car les effets des deux substances se cumulent dangereusement. La raison : des réflexes amoindris, doublés d’un sentiment de désinhibition, qui peuvent avoir des conséquences graves !'
+      explanation:
+        'Le mélange drogue/cannabis est délétère car les effets des deux substances se cumulent dangereusement. La raison : des réflexes amoindris, doublés d’un sentiment de désinhibition, qui peuvent avoir des conséquences graves !',
     },
     done: false,
   },
   {
     question_index: 8,
-    title: 'Sur les boîtes des médicaments, combien y a-t-il de niveaux de risque représentés par un pictogramme triangulaire ?',
-    answers: [
-      '3 niveaux',
-      '5 niveaux',
-      '8 niveaux',
-    ],
+    title:
+      'Sur les boîtes des médicaments, combien y a-t-il de niveaux de risque représentés par un pictogramme triangulaire ?',
+    answers: ['3 niveaux', '5 niveaux', '8 niveaux'],
     right_answer: {
       index: 0,
-      explanation: 'Vision, motricité, audition… Certains médicaments peuvent amoindrir les capacités des conducteurs ! Pour identifier le niveau de risque, des pictogrammes assortis de messages sont imprimés sur leurs boîtes. Jaune : soyez prudent ; orange : soyez très prudent, demandez l’avis du médecin avant de conduire ; rouge : attention danger, la conduite est impossible.'
+      explanation:
+        'Vision, motricité, audition… Certains médicaments peuvent amoindrir les capacités des conducteurs ! Pour identifier le niveau de risque, des pictogrammes assortis de messages sont imprimés sur leurs boîtes. Jaune : soyez prudent ; orange : soyez très prudent, demandez l’avis du médecin avant de conduire ; rouge : attention danger, la conduite est impossible.',
     },
     done: false,
   },
   {
     question_index: 9,
-    title: '5 heures de sommeil ou moins la veille d’un départ multiplient le risque d’accident par :',
-    answers: [
-      '2',
-      '3',
-      '4',
-    ],
+    title:
+      '5 heures de sommeil ou moins la veille d’un départ multiplient le risque d’accident par :',
+    answers: ['2', '3', '4'],
     right_answer: {
       index: 1,
-      explanation: 'La fatigue et la somnolence au volant augmentent le risque d’accident. Avant de faire un long trajet, il convient de dormir suffisamment de manière à ne pas constituer une dette de sommeil. De même, il est déconseillé de partir après une journée de travail sans s’être reposé.'
+      explanation:
+        'La fatigue et la somnolence au volant augmentent le risque d’accident. Avant de faire un long trajet, il convient de dormir suffisamment de manière à ne pas constituer une dette de sommeil. De même, il est déconseillé de partir après une journée de travail sans s’être reposé.',
     },
     done: false,
   },
   {
     question_index: 10,
     title: 'La fatigue est la 1re cause d’accident mortel sur l’autoroute :',
-    answers: [
-      'Vrai',
-      'Faux',
-    ],
+    answers: ['Vrai', 'Faux'],
     right_answer: {
       index: 0,
-      explanation: 'La somnolence est un vrai danger, particulièrement sur l’autoroute : elle y a été responsable d’un quart des accidents mortels entre 2015 et 2019.'
+      explanation:
+        'La somnolence est un vrai danger, particulièrement sur l’autoroute : elle y a été responsable d’un quart des accidents mortels entre 2015 et 2019.',
     },
     done: false,
   },
   {
     question_index: 11,
-    title: 'À quelle fréquence est-il conseillé au conducteur de faire une pause lors d’un long trajet en voiture sur la route ?',
-    answers: [
-      '2 heures',
-      '3 heures',
-    ],
+    title:
+      'À quelle fréquence est-il conseillé au conducteur de faire une pause lors d’un long trajet en voiture sur la route ?',
+    answers: ['2 heures', '3 heures'],
     right_answer: {
       index: 0,
-      explanation: 'Le risque d’avoir un accident est 8 fois plus important lorsqu’on est somnolent ! Faire des pauses s’impose toutes les 2 heures, et plus si la fatigue se fait sentir !'
+      explanation:
+        'Le risque d’avoir un accident est 8 fois plus important lorsqu’on est somnolent ! Faire des pauses s’impose toutes les 2 heures, et plus si la fatigue se fait sentir !',
     },
     done: false,
   },
   {
     question_index: 12,
-    title: 'Être contrôlé avec une alcoolémie supérieure à 0,8 g par litre de sang est un délit puni :',
+    title:
+      'Être contrôlé avec une alcoolémie supérieure à 0,8 g par litre de sang est un délit puni :',
     answers: [
       'jusqu’à 1 an d’emprisonnement',
       'jusqu’à 2 ans d’emprisonnement',
@@ -188,26 +174,27 @@ export const securiteRoutiere = [
     ],
     right_answer: {
       index: 1,
-      explanation: 'Au-delà du seuil des 0,8 g d’alcool par litre de sang, il s’agit d’un délit passible de 2 ans d’emprisonnement qui, en plus d’un retrait de 6 points et d’une amende de 4 500 euros, s’accompagne d’un stage obligatoire de sensibilisation à la sécurité routière aux frais du contrevenant.'
+      explanation:
+        'Au-delà du seuil des 0,8 g d’alcool par litre de sang, il s’agit d’un délit passible de 2 ans d’emprisonnement qui, en plus d’un retrait de 6 points et d’une amende de 4 500 euros, s’accompagne d’un stage obligatoire de sensibilisation à la sécurité routière aux frais du contrevenant.',
     },
     done: false,
   },
   {
     question_index: 13,
-    title: 'À quelle fréquence est-il conseillé au conducteur de faire une pause lors d’un long trajet en voiture sur la route ?',
-    answers: [
-      '2 heures',
-      '3 heures',
-    ],
+    title:
+      'À quelle fréquence est-il conseillé au conducteur de faire une pause lors d’un long trajet en voiture sur la route ?',
+    answers: ['2 heures', '3 heures'],
     right_answer: {
       index: 0,
-      explanation: 'Le risque d’avoir un accident est 8 fois plus important lorsqu’on est somnolent ! Faire des pauses s’impose toutes les 2 heures, et plus si la fatigue se fait sentir !'
+      explanation:
+        'Le risque d’avoir un accident est 8 fois plus important lorsqu’on est somnolent ! Faire des pauses s’impose toutes les 2 heures, et plus si la fatigue se fait sentir !',
     },
     done: false,
   },
   {
     question_index: 14,
-    title: 'Sur autoroute, diminuer sa vitesse de 10 km/h en passant de 130 à 120 km/h sur un trajet de 500 km permet-il d’économiser du carburant ?',
+    title:
+      'Sur autoroute, diminuer sa vitesse de 10 km/h en passant de 130 à 120 km/h sur un trajet de 500 km permet-il d’économiser du carburant ?',
     answers: [
       'non, aucune différence de consommation',
       'oui, environ 4 litres de carburant',
@@ -215,29 +202,29 @@ export const securiteRoutiere = [
     ],
     right_answer: {
       index: 1,
-      explanation: 'Réduire sa vitesse de 10 km/h sur autoroute en passant de 130 à 120 km/h permet d’économiser sur 500 km entre 3,5 et 4,5 litres de carburant (selon la motorisation et le type de véhicule), et près de 12 kg de CO2. En plus, abaisser la vitesse permet de diminuer le nombre des accidents de la route et leur gravité. En effet, la distance d’arrêt est réduite et permet d’éviter les obstacles imprévus.'
+      explanation:
+        'Réduire sa vitesse de 10 km/h sur autoroute en passant de 130 à 120 km/h permet d’économiser sur 500 km entre 3,5 et 4,5 litres de carburant (selon la motorisation et le type de véhicule), et près de 12 kg de CO2. En plus, abaisser la vitesse permet de diminuer le nombre des accidents de la route et leur gravité. En effet, la distance d’arrêt est réduite et permet d’éviter les obstacles imprévus.',
     },
     done: false,
   },
   {
     question_index: 15,
-    title: 'Avoir des pneus sous-gonflés peut entraîner une surconsommation de carburant.',
-    answers: [
-      'Vrai',
-      'Faux',
-    ],
+    title:
+      'Avoir des pneus sous-gonflés peut entraîner une surconsommation de carburant.',
+    answers: ['Vrai', 'Faux'],
     right_answer: {
       index: 0,
-      explanation: 'Vrai, un sous-gonflage des pneus entraîne une surconsommation de carburant. Pour 0,3 bar de moins que la pression recommandée, on atteint 1,2 % de consommation en plus, pour 0,5 bar en moins, 2,4 % de consommation en plus. Un pneu mal gonflé peut également entraîner une mauvaise adhérence à la route et augmenter le risque d’éclatement. Entretenir correctement ses pneus permet de rouler en toute sécurité et de les garder plus longtemps.'
+      explanation:
+        'Vrai, un sous-gonflage des pneus entraîne une surconsommation de carburant. Pour 0,3 bar de moins que la pression recommandée, on atteint 1,2 % de consommation en plus, pour 0,5 bar en moins, 2,4 % de consommation en plus. Un pneu mal gonflé peut également entraîner une mauvaise adhérence à la route et augmenter le risque d’éclatement. Entretenir correctement ses pneus permet de rouler en toute sécurité et de les garder plus longtemps.',
     },
     done: false,
-  }
+  },
 ];
 
-export const questionsLorem2 = [
+export const questionsLorem = [
   {
     question_index: 0,
-    title: 'Question 1 lorem 2',
+    title: 'Question 1',
     answers: [
       'Lorem ipsum dolor sit amet',
       'Consectetur adipiscing elit',
@@ -246,7 +233,8 @@ export const questionsLorem2 = [
     ],
     right_answer: {
       index: 0,
-      explanation: 'Vrai, un sous-gonflage des pneus entraîne une surconsommation de carburant. Pour 0,3 bar de moins que la pression recommandée, on atteint 1,2 % de consommation en plus, pour 0,5 bar en moins, 2,4 % de consommation en plus. Un pneu mal gonflé peut également entraîner une mauvaise adhérence à la route et augmenter le risque d’éclatement. Entretenir correctement ses pneus permet de rouler en toute sécurité et de les garder plus longtemps.'
+      explanation:
+        'Vrai, un sous-gonflage des pneus entraîne une surconsommation de carburant. Pour 0,3 bar de moins que la pression recommandée, on atteint 1,2 % de consommation en plus, pour 0,5 bar en moins, 2,4 % de consommation en plus. Un pneu mal gonflé peut également entraîner une mauvaise adhérence à la route et augmenter le risque d’éclatement. Entretenir correctement ses pneus permet de rouler en toute sécurité et de les garder plus longtemps.',
     },
     done: false,
   },
@@ -261,7 +249,8 @@ export const questionsLorem2 = [
     ],
     right_answer: {
       index: 0,
-      explanation: 'Vrai, un sous-gonflage des pneus entraîne une surconsommation de carburant. Pour 0,3 bar de moins que la pression recommandée, on atteint 1,2 % de consommation en plus, pour 0,5 bar en moins, 2,4 % de consommation en plus. Un pneu mal gonflé peut également entraîner une mauvaise adhérence à la route et augmenter le risque d’éclatement. Entretenir correctement ses pneus permet de rouler en toute sécurité et de les garder plus longtemps.'
+      explanation:
+        'Vrai, un sous-gonflage des pneus entraîne une surconsommation de carburant. Pour 0,3 bar de moins que la pression recommandée, on atteint 1,2 % de consommation en plus, pour 0,5 bar en moins, 2,4 % de consommation en plus. Un pneu mal gonflé peut également entraîner une mauvaise adhérence à la route et augmenter le risque d’éclatement. Entretenir correctement ses pneus permet de rouler en toute sécurité et de les garder plus longtemps.',
     },
     done: false,
   },
